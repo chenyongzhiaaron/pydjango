ce746811e3dbdf1eefb52bc06a79ba8d6605c8e9
262667641@qq.com <262667641@qq.com>
2020-01-16 15:57:12 +0800

    first commit

---
+++ apitest/templates/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/1/15 16:59
+# @Author : kira
+# @Email : 262667641@qq.com
+# @File : __init__.py.py
+# @Project : autotest
+
+
+if __name__ == '__main__':
+    pass
